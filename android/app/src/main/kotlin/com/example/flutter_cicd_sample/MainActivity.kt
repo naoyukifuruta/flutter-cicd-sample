4459625efc1f45946965ad5473c7e45bdbbec6bd
naoyukifuruta <naoyukifuruta@gmail.com>
2025-04-26 14:50:01 +0900

    flutter upgade

--- android/app/src/main/kotlin/com/example/flutter_cicd_sample/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_cicd_sample/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.flutter_cicd_sample
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,4 +1,4 @@
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('2 * 5 = 10', () {
